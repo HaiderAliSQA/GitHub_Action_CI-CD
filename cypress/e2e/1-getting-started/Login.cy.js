38791acdd4d93c4f2d1c3e887500f474f10997f0
HaiderAliSQA <hafizhaideraliuet@gmail.com>
2025-08-26 12:48:48 +0500

    first commit

---
+++ cypress/e2e/1-getting-started/Login.cy.js
@@ -0,0 +1,11 @@
+describe('OpenHRM Login', () => {
+  it('should login to OpenHRM', () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get('input[placeholder="Username"]').type("Admin");
+    cy.get('input[placeholder="Password"]').type("admin123");
+    cy.get('button[type="submit"]').click();
+    cy.get('.oxd-userdropdown-tab').should('be.visible')
+  })
+})  
