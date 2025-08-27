a8819995e8cd9fd71c747bd041a1541d957c2c60
HaiderAliSQA <hafizhaideraliuet@gmail.com>
2025-08-27 09:23:54 +0500

    CI_CD Pipe line ka flow

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,11 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/html",
+    overwrite: true, // This will overwrite instead of creating new
+    html: true,
+    json: false,
+  },
 });
